44a04d5655ee28a61c3d955c67cb1b5298cc116c
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-24 10:50:17 +0530

    get jpa library to optimize queries

--- src/main/java/com/rhinopartners/Springproject/entity/Sample.java
+++ src/main/java/com/rhinopartners/Springproject/entity/Sample.java
@@ -1,14 +1,25 @@
 package com.rhinopartners.Springproject.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Builder
+@Entity
+@Table(name="sample_one")
 public class Sample {
     int id;
     String reference;
--- src/test/java/com/rhinopartners/Springproject/dao/impl/SampleDAOImplTest.java
+++ src/test/java/com/rhinopartners/Springproject/dao/impl/SampleDAOImplTest.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -22,9 +21,6 @@ public class SampleDAOImplTest {
     private PreparedStatement stm;
     @Mock
     private int affectedRows;
-
-    private TestObject TestObject;
-
 
 
     @Test
