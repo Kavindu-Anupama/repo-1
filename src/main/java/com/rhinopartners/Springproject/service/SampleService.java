4dd249f47d6afe520bc0f0e660c692bd7af87613
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-25 14:42:57 +0530

    used response status exception class

--- src/main/java/com/rhinopartners/Springproject/service/SampleService.java
+++ src/main/java/com/rhinopartners/Springproject/service/SampleService.java
@@ -1,13 +1,15 @@
-package com.rhinopartners.Springproject.dao;
+package com.rhinopartners.Springproject.service;
 
 import java.util.List;
 
 import com.rhinopartners.Springproject.dto.SampleDTO;
 
-public interface SampleDAO {
+public interface SampleService {
     void saveSample(SampleDTO sample);
     void updateSample(SampleDTO sample);
     void deleteSample(int id);
     SampleDTO getSample(int id);
     List<SampleDTO> findSample(String query);    
 }
+
+//this interface created to achieve loosely coupelling for the services. 
--- src/main/java/com/rhinopartners/Springproject/service/impl/SampleServiceImpl.java
+++ src/main/java/com/rhinopartners/Springproject/service/impl/SampleServiceImpl.java
@@ -2,20 +2,33 @@ package com.rhinopartners.Springproject.service.impl;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.rhinopartners.Springproject.dto.SampleDTO;
+import com.rhinopartners.Springproject.repository.SampleRepository;
 import com.rhinopartners.Springproject.service.SampleService;
 
 @Service
 @Transactional
 public class SampleServiceImpl implements SampleService {
 
+    private final SampleRepository sampleRepository;
+
+    public SampleServiceImpl(SampleRepository sampleRepository){
+        this.sampleRepository=sampleRepository;
+        // to initialize sampleRepository constant in the begining
+    }
+
     @Override
     public void saveSample(SampleDTO sample) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'saveSample'");
+        if (sampleRepository.existsById(sample.getId())) {
+            System.out.println("Failed to save. giver id already exists.");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Sample already exists.");
+        }
+
     }
 
     @Override
