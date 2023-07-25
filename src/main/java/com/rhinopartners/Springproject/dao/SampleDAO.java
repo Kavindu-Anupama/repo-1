d6b0fde64bd7635e8d68bc2e67f28e1f1c0189d6
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-25 09:20:16 +0530

    corrections

--- src/main/java/com/rhinopartners/Springproject/dao/SampleDAO.java
+++ src/main/java/com/rhinopartners/Springproject/dao/SampleDAO.java
@@ -1,11 +1,9 @@
 package com.rhinopartners.Springproject.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.rhinopartners.Springproject.entity.Sample;
-
-@Repository
-public interface SampleDAO extends JpaRepository<Sample, Integer> {
+public interface SampleDAO {
+    void saveSample();
+    void updateSample();
+    void deleteSample(int id);
     
 }
