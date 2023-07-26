5700b25d70f148b22d43b717875c19bccd6b816d
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-26 11:37:50 +0530

    implemented save sample

--- src/main/java/com/rhinopartners/Springproject/api/HttpControllerReq.java
+++ src/main/java/com/rhinopartners/Springproject/api/HttpControllerReq.java
@@ -1,36 +1,35 @@
 package com.rhinopartners.Springproject.api;
 
-import java.util.ArrayList;
-
-import org.hibernate.mapping.List;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rhinopartners.Springproject.entity.Sample;
+import com.rhinopartners.Springproject.dto.SampleDTO;
+import com.rhinopartners.Springproject.service.SampleService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/app")
 public class HttpControllerReq {
 
-    @GetMapping("/getAllSamples")
-    public ResponseEntity<List<Sample>> getAllSamples(){
-        try {
-            List<Sample> sampleList = new ArrayList<>();
-            
-        }
+    private final SampleService sampleService;
+
+    public HttpControllerReq(SampleService sampleService) {
+        this.sampleService=sampleService;
     }
 
-    @PostMapping
-    public ResponseEntity<<Sample>> addSample() {
-        
-        
+    @ResponseStatus(HttpStatus.CREATED)  //201
+    @PostMapping(consumes = "application/json")
+    public void addSample(@RequestBody @Validated SampleDTO sampleDTO) {
+        sampleService.saveSample(sampleDTO);
     }
 
     @GetMapping
