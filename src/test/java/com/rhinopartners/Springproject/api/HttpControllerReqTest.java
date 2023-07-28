2bf00effda0e664ecc525fa3e5061dc3e41f8a4d
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-28 16:29:11 +0530

    Unit testing for saveSample on the progress

--- src/test/java/com/rhinopartners/Springproject/api/HttpControllerReqTest.java
+++ src/test/java/com/rhinopartners/Springproject/api/HttpControllerReqTest.java
@@ -1,11 +1,51 @@
 package com.rhinopartners.Springproject.api;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import javax.ws.rs.core.MediaType;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rhinopartners.Springproject.dto.SampleDTO;
+import com.rhinopartners.Springproject.entity.Sample;
+import com.rhinopartners.Springproject.service.SampleService;
+
+
+@WebMvcTest(HttpControllerReq.class)
 public class HttpControllerReqTest {
-    @Test
-    void testAddSample() {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SampleService sampleService;
+
+    @MockBean
+    private ObjectMapper objectMapper;
+
+    @Test
+    void givenSampleObject_whenCreateSample_thenReturnSavedSample() throws Exception {
+        
+        Sample sample = Sample.builder()
+                                .id(5)
+                                .reference("myTest")
+                                .amount(10)
+                                .createAs("aTest")
+                                .feedback("test went good")
+                                .build();
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
     }
 
     @Test
