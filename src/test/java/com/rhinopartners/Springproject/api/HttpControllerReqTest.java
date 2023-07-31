5662224ab9ffc7ada67f3437c5188fc94a0b59e6
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-31 15:36:30 +0530

    resolved import bugs

--- src/test/java/com/rhinopartners/Springproject/api/HttpControllerReqTest.java
+++ src/test/java/com/rhinopartners/Springproject/api/HttpControllerReqTest.java
@@ -4,28 +4,19 @@ import static org.mockito.ArgumentMatchers.any;
 
 import javax.ws.rs.core.MediaType;
 
-import org.assertj.core.api.Condition.Status;
 import org.junit.jupiter.api.Test;
-import org.postgresql.core.ResultHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.mock.http.client.MockClientHttpRequest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rhinopartners.Springproject.dto.SampleDTO;
-import com.rhinopartners.Springproject.entity.Sample;
 import com.rhinopartners.Springproject.service.SampleService;
-
-import jakarta.servlet.ServletContext;
+import static org.mockito.BDDMockito.given;
 
 @WebMvcTest(HttpControllerReq.class)
 public class HttpControllerReqTest {
@@ -42,7 +33,7 @@ public class HttpControllerReqTest {
     @Test
     void givenSampleObject_whenCreateSample_thenReturn201() throws Exception {
 
-        // //given - precondition
+        // given - precondition
         SampleDTO sampleDTO = SampleDTO.builder()
                 .id(5)
                 .reference("myTest")
@@ -50,32 +41,27 @@ public class HttpControllerReqTest {
                 .createAs("aTest")
                 .feedback("test went good")
                 .build();
+        given(sampleService.saveSample(any(SampleDTO.class)))
+                .willAnswer((invocation)-> invocation.getArgument(0));
 
-        mockMvc.perform(post("/"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeHasErrors("sampleDTO"));
-
-        // RequestBuilder requestBuilder = MockMvcRequestBuilders
-        // .post("/").contentType(MediaType.APPLICATION_JSON)
-        // .content(objectMapper.writeValueAsString(sampleDTO));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+        .post("/").contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(sampleDTO));
         // Builds a MockHttpServletRequest.
 
         // when - action
-        // ResultActions resultActions = mockMvc.perform(requestBuilder);
-        // Allows applying actions, such as expectations, on the result of an executed
-        // request.
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
+        // Allows applying actions, such as expectations, on the result of an executed request.
 
         // then - varification
-        // resultActions.andExpect();
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
 
     }
 
     @Test
     void givenSampleId_whenDeleteSample_thenReturn204() throws Exception {
 
-        // given - preconditions
-        int sampleId = 5;
-        given(sampleService).deleteSample(sampleId);
+        
     }
 
     @Test
