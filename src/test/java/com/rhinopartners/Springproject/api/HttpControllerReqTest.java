7bd5e717dfbf3d146481e776c95f486c71a27e04
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-31 11:18:18 +0530

    initial setup for BDD using cucumber framework

--- src/test/java/com/rhinopartners/Springproject/api/HttpControllerReqTest.java
+++ src/test/java/com/rhinopartners/Springproject/api/HttpControllerReqTest.java
@@ -4,22 +4,27 @@ import static org.mockito.ArgumentMatchers.any;
 
 import javax.ws.rs.core.MediaType;
 
+import org.assertj.core.api.Condition.Status;
 import org.junit.jupiter.api.Test;
+import org.postgresql.core.ResultHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.http.client.MockClientHttpRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rhinopartners.Springproject.dto.SampleDTO;
 import com.rhinopartners.Springproject.entity.Sample;
 import com.rhinopartners.Springproject.service.SampleService;
+
+import jakarta.servlet.ServletContext;
 
 
 @WebMvcTest(HttpControllerReq.class)
@@ -37,15 +42,28 @@ public class HttpControllerReqTest {
     @Test
     void givenSampleObject_whenCreateSample_thenReturnSavedSample() throws Exception {
         
-        Sample sample = Sample.builder()
+        //given - precondition
+        SampleDTO sampleDTO = SampleDTO.builder()
                                 .id(5)
                                 .reference("myTest")
                                 .amount(10)
                                 .createAs("aTest")
                                 .feedback("test went good")
                                 .build();
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/");
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                                        .post("/").contentType(MediaType.APPLICATION_JSON)
+                                        .content(objectMapper.writeValueAsString(sampleDTO));
+                                        // Builds a MockHttpServletRequest.
+
+        //when - action
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
+        //Allows applying actions, such as expectations, on the result of an executed request.
+        
+        //then - varification
+        ResultMatcher matcher;
+        resultActions.andExpect(status().isOk())
+        
+        
     }
 
     @Test
