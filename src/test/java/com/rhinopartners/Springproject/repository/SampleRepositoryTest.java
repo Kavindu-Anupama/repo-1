9c62e116c7babbdb2cf8c7493d82ff10e17d7185
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-08-14 15:46:17 +0530

    fixed bugs for savesampletest

--- src/test/java/com/rhinopartners/Springproject/repository/SampleRepositoryTest.java
+++ src/test/java/com/rhinopartners/Springproject/repository/SampleRepositoryTest.java
@@ -4,10 +4,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Configuration;
 
 import com.rhinopartners.Springproject.entity.Sample;
 
-@DataJpaTest
+
+@Configuration
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SampleRepositoryTest {
 
     @Autowired
