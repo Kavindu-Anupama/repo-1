6bb24d3c06b1ea904fefff3b0af2eee7daa51c66
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-08-21 15:23:29 +0530

    fixed bugs on post request

--- src/main/java/com/rhinopartners/Springproject/dto/SampleDTO.java
+++ src/main/java/com/rhinopartners/Springproject/dto/SampleDTO.java
@@ -16,12 +16,12 @@ public class SampleDTO implements Serializable {
     @NotNull(message = "id can not be empty")
     private int id;
     @NotBlank(message = "Reference can not be empty")
-    @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid reference name")
+    @Pattern(regexp = "^[A-Za-z0-9 ]+$", message = "Invalid reference name")
     private String reference;
     @NotNull(message = "Amount can not be empty")
     private int amount;
     @NotBlank(message = "Created name can not be empty")
-    @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid created name")
+    @Pattern(regexp = "^[A-Za-z0-9 ]+$", message = "Invalid created name")
     private String createAs;
     @NotBlank(message = "Feedback can not be empty")
     @Pattern(regexp = "^[A-Za-z0-9 ]+$", message = "Invalid feedback")
